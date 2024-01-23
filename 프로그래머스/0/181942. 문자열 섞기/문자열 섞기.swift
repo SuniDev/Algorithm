a6f4f515444789ffc1ee57a4eb87e27505e61b98
SuniDev <56523702+SuniDev@users.noreply.github.com>
2024-01-23 21:12:50 +0900

    [level 0] Title: 문자열 섞기, Time: 0.12 ms, Memory: 16.2 MB -BaekjoonHub

--- 프로그래머스/0/181942. 문자열 섞기/문자열 섞기.swift
+++ 프로그래머스/0/181942. 문자열 섞기/문자열 섞기.swift
@@ -1,11 +1,5 @@
 import Foundation
 
 func solution(_ str1:String, _ str2:String) -> String {
-    var out = ""
-    let mapStr = Array(str2)
-    for (index, char) in str1.enumerated() {
-        out += "\(char)\(mapStr[index])"
-    }
-    
-    return out
+    return zip(str1, str2).map { "\($0)\($1)" }.joined()
 }
