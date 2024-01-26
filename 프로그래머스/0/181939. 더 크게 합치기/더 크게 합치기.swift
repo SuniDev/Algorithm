2b392ad438cb87d1a5def9df21dbc801d3b403f2
SuniDev <56523702+SuniDev@users.noreply.github.com>
2024-01-26 21:47:34 +0900

    [level 0] Title: 더 크게 합치기, Time: 0.04 ms, Memory: 16.3 MB -BaekjoonHub

--- 프로그래머스/0/181939. 더 크게 합치기/더 크게 합치기.swift
+++ 프로그래머스/0/181939. 더 크게 합치기/더 크게 합치기.swift
@@ -1,7 +1,5 @@
 import Foundation
 
 func solution(_ a:Int, _ b:Int) -> Int {
-    let ab: Int = Int(String(a)+String(b)) ?? 0
-    let ba: Int = Int(String(b)+String(a)) ?? 0
-    return ab >= ba ? ab : ba
+    return max(Int(String(a)+String(b)) ?? 0, Int(String(b)+String(a)) ?? 0)
 }
