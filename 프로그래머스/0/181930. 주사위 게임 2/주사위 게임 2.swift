66e4856749f21a36d031de5b6ff3aa1f116ad696
SuniDev <56523702+SuniDev@users.noreply.github.com>
2024-02-14 18:10:33 +0900

    [level 0] Title: 주사위 게임 2, Time: 0.04 ms, Memory: 16.4 MB -BaekjoonHub

--- 프로그래머스/0/181930. 주사위 게임 2/주사위 게임 2.swift
+++ 프로그래머스/0/181930. 주사위 게임 2/주사위 게임 2.swift
@@ -1,25 +1,12 @@
 import Foundation
 
 func solution(_ a:Int, _ b:Int, _ c:Int) -> Int {
-    var x = 1
+    var cnt = Set([a,b,c]).count
     
-    if a == b && b == c {
-        x = 3
-    } else {
-        if a == b { x += 1 }
-        if a == c { x += 1 }
-        if b == c { x += 1 }
+    switch cnt {
+        case 3: return a + b + c
+        case 2: return (a + b + c) * ( a * a + b * b + c * c)
+        case 1: return (a + b + c) * ( a * a + b * b + c * c) * ( a * a * a + b * b * b + c * c * c)
+        default: return 0
     }
-    
-    var score = 1
-    for i in 1...x {
-        var sum = 0
-        sum += Int(pow(Float(a), Float(i)))
-        sum += Int(pow(Float(b), Float(i)))
-        sum += Int(pow(Float(c), Float(i)))
-        
-        score *= sum
-    }
-    
-    return score
 }
