59f0ace904ff9b03559679cae7df23849147bb25
SuniDev <56523702+SuniDev@users.noreply.github.com>
2024-03-13 20:48:51 +0900

    [level 0] Title: 배열 만들기 2, Time: 115.29 ms, Memory: 16.6 MB -BaekjoonHub

--- 프로그래머스/0/181921. 배열 만들기 2/배열 만들기 2.swift
+++ 프로그래머스/0/181921. 배열 만들기 2/배열 만들기 2.swift
@@ -1,16 +1,18 @@
 import Foundation
 
 func solution(_ l:Int, _ r:Int) -> [Int] {
-    var arr = [Int]()
-    (l...r).forEach {
-        if $0 % 5 == 0 {
-            var str = String($0)
-            str = str.replacingOccurrences(of: "5", with: "")
-            str = str.replacingOccurrences(of: "0", with: "")
-            if str.isEmpty {
-                arr.append($0)
-            }
-        }
-    }
-    return arr.isEmpty ? [-1] : arr
+    let answer = (l...r).map { $0 }.filter { Set(String($0)).isSubset(of: ["0", "5"]) }
+    return answer.isEmpty ? [-1] : answer
+    // var arr = [Int]()
+    // (l...r).forEach {
+    //     if $0 % 5 == 0 {
+    //         var str = String($0)
+    //         str = str.replacingOccurrences(of: "5", with: "")
+    //         str = str.replacingOccurrences(of: "0", with: "")
+    //         if str.isEmpty {
+    //             arr.append($0)
+    //         }
+    //     }
+    // }
+    // return arr.isEmpty ? [-1] : arr
 }
